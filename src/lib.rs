5161b883ea60f8acd102f53fa97c660a3ab47849
Paulo <jimgr@riseup.net>
2024-08-08 09:03:50 +0200

    Init

---
+++ src/lib.rs
@@ -0,0 +1,60 @@
+use serde_derive::{Deserialize, Serialize};
+
+use reqwest::Client;
+
+#[derive(Deserialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct Token {
+    pub token: String,
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct ConfigJson{
+    pub amnz_ovc_config: AmneziaConfig,
+    pub outline_config: OutlineConfig,
+    pub user_name: String,
+    pub wireguard_config: WireguardConfig
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct AmneziaConfig {
+    pub file_content: String,
+    pub file_name: String,
+    pub tonnel_name: String
+}
+
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct OutlineConfig {
+    pub access_key: String
+}
+
+#[derive(Deserialize, Serialize)]
+#[serde(rename_all = "PascalCase")]
+pub struct WireguardConfig {
+    pub file_content: String,
+    pub file_name: String,
+    pub tonnel_name: String
+}
+
+/// Every "brigade owner" has unique prefix in address. We get it using this function
+pub async fn get_base_url() -> Result<String, Box<dyn std::error::Error>>{
+    Ok(reqwest::get("http://vpn.works").await?.url().as_str().to_string())
+}
+
+/// Get token using post request to prefix.vpn.works/token
+pub async fn get_token(client: &Client, base_url: &String) -> Result<Token, Box<dyn std::error::Error>>{
+    let token = client.post(format!("{}/token",base_url)).send().await?.json::<Token>().await?;
+    Ok(token)
+}
+
+/// Get configs for amnezia, outline, wireguard
+pub async fn get_config(client: &Client) -> Result<ConfigJson, Box<dyn std::error::Error>>{
+    let base_url = get_base_url().await?;
+    let token = get_token(&client, &base_url).await?;
+
+    Ok(client.post(format!("{}/user",base_url)).bearer_auth(token.token).send().await?.json::<ConfigJson>().await?)
+}
---
+++ src/main.rs
@@ -0,0 +1,124 @@
+use std::{fs::File, str::FromStr};
+use std::fs;
+use std::io::prelude::*;
+use reqwest::Proxy;
+use serde_derive::{Deserialize, Serialize};
+use vpngen_lib::get_config;
+use std::path::PathBuf;
+
+use clap::Parser;
+
+
+use ss_to_json_outline;
+use amcofixer_lib;
+
+/*fn print_format(config: &ConfigJson, template_text: &String) -> String {
+    let text_to_return = template_text
+        .replace("[!outline_config]", &config.outline_config.access_key)
+        .replace("[!amnezia_config]", &config.amnz_ovc_config.file_content)
+        .replace("[!amnezia_path]", &config.amnz_ovc_config.file_name)
+        .replace("[!wireguard_config]", &config.wireguard_config.file_content)
+        .replace("[!wireguard_path]", &config.wireguard_config.file_name);
+    text_to_return
+}   */
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, value_name = "FILE")]
+    template: Option<PathBuf>, //Test option.
+    #[arg(short, long, value_name = "FILE")]
+    config_dir: Option<PathBuf>,
+    #[arg(short, long)]
+    shadowsocks: bool,
+    #[arg(short, long)]
+    amnezia: bool,
+    #[arg(short='f', long)]
+    amnezia_fixer: bool,
+    #[arg(short, long)]
+    wireguard: bool,
+    #[arg(long)]
+    all: bool,
+    /// Usage example: vpngen_lib --socks5 127.0.0.1:9052
+    #[arg(long)]
+    socks5: Option<String>
+}
+
+fn save_to_file(file_name: &String, file_content: &String) -> std::io::Result<()> {
+    let mut file = File::create(file_name.as_str())?;
+    file.write_all(file_content.as_bytes())?;
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct TemplateText{
+    outline: String,
+    shadowsocks: String,
+    amnezia: String,
+    amnezia_fixer: String,
+    wireguard: String
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+    
+    let template_text = match  cli.template {
+        Some(template_path) => {
+            let data = fs::read_to_string(template_path)?;
+            serde_json::from_str(&data)?
+        }
+        None => TemplateText{
+            outline: "Outline: [!outline_config]".to_string(),
+            shadowsocks: "ShadowSocks: [!shadowsocks]".to_string(),
+            amnezia: "Amnezia: [!amnezia_path]".to_string(),
+            amnezia_fixer: "Fixed amnezia file: [!amnezia_path]".to_string(),
+            wireguard: "Wireguard: [!wireguard_path]".to_string()
+        }
+    };
+
+    let config_dir= match  cli.config_dir {
+        Some(template_path) => template_path,
+        None => PathBuf::from_str("./vpn_config").unwrap()   
+    }.to_str().unwrap().to_string();
+
+    let client = match cli.socks5 {
+        Some(data) => reqwest::Client::builder().proxy(Proxy::http(data)?).build()?,
+        None => reqwest::Client::new()
+    };
+    
+    
+    let config = get_config(&client).await?;
+
+    let mut printable_text: String = format!("### {}\n\n", config.user_name);
+
+    printable_text.push_str(template_text.outline.replace("[!outline_config]", &config.outline_config.access_key).as_str());
+    if cli.shadowsocks || cli.all{
+        printable_text.push('\n');
+        printable_text.push('\n');
+        printable_text.push_str(&template_text.shadowsocks.replace("[!shadowsocks]", &format!("{}.json", &config.user_name).as_str()));
+        let shadowsocks_config = serde_json::to_string(&ss_to_json_outline::ss_to_json(config.outline_config.access_key).unwrap()).unwrap();
+        save_to_file(&format!("{}/{}.json", config_dir, config.user_name), &shadowsocks_config)?;
+    }
+    
+    if cli.amnezia || cli.all{
+        printable_text.push('\n');
+        printable_text.push('\n');
+        printable_text.push_str(template_text.amnezia.replace("[!amnezia_path]", &config.amnz_ovc_config.file_name).as_str());
+        save_to_file(&format!("{}/{}", config_dir, config.amnz_ovc_config.file_name), &config.amnz_ovc_config.file_content)?;
+    }
+    if cli.amnezia_fixer || cli.all{
+        printable_text.push('\n');
+        printable_text.push('\n');
+        printable_text.push_str(&template_text.amnezia_fixer.replace("[!amnezia_path]", &format!("fixed_{}", &config.amnz_ovc_config.file_name).as_str()));
+        save_to_file(&format!("{}/fixed_{}", config_dir, config.amnz_ovc_config.file_name), &amcofixer_lib::fixer(&config.amnz_ovc_config.file_content))?;
+    }
+    if cli.wireguard || cli.all{
+        printable_text.push('\n');
+        printable_text.push('\n');
+        printable_text.push_str(template_text.wireguard.replace("[!wireguard_path]", &config.wireguard_config.file_name).as_str());
+        save_to_file(&format!("{}/{}", config_dir, config.wireguard_config.file_name), &config.wireguard_config.file_content)?;
+    }
+    println!("{}", printable_text);
+    Ok(())
+}
